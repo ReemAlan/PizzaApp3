35d08599c150cbd9338fc3ef1f6f167607b11fce
HannahHatem <hannah@silverkeytech.com>
2021-09-06 14:49:25 +0200

    Adding forms for ordering

--- PizzaClient.Razor/Pages/Menu.cshtml.cs
+++ PizzaClient.Razor/Pages/Menu.cshtml.cs
@@ -2,52 +2,63 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Text.Json.Nodes;
 using System.Text.Json;
+using System.Collections.Generic;
+using PizzaClient.Razor.DTOs;
+using Microsoft.Extensions.Logging;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
-namespace PizzaClient.Razor.Pages;
-
-public class MenuModel : PageModel
+namespace PizzaClient.Razor.Pages
 {
-    public IDictionary<string, double> Sizes { get; set; } = new Dictionary<string, double>();
-    public IDictionary<string, double> Dough { get; set; } = new Dictionary<string, double>();
-    public IDictionary<string, double> Toppings { get; set; } = new Dictionary<string, double>();
-    public IDictionary<string, double> Sauces { get; set; } = new Dictionary<string, double>();
-
-    private readonly IHttpClientFactory _clientFactory;
-
-    public MenuModel(IHttpClientFactory clientFactory)
+    public class MenuModel : PageModel
     {
-        _clientFactory = clientFactory;
-    }
+        public IDictionary<string, double> Sizes { get; set; } = new Dictionary<string, double>();
+        public IDictionary<string, double> Dough { get; set; } = new Dictionary<string, double>();
+        public IDictionary<string, double> Toppings { get; set; } = new Dictionary<string, double>();
+        public IDictionary<string, double> Sauces { get; set; } = new Dictionary<string, double>();
 
-    public async Task OnGetAsync()
-    {
-        var client = _clientFactory.CreateClient("localhost");
-        string response = await client.GetStringAsync("menu");
-        JsonObject? menu = JsonNode.Parse(response).AsObject();
+        private readonly IHttpClientFactory _clientFactory;
 
-        List<JsonNode?> sizes = menu["sizeTable"].AsArray().ToList();
-        foreach (var obj in sizes)
+        public MenuModel(IHttpClientFactory clientFactory)
         {
-            Sizes.Add((string)obj["option"], (double)obj["multiplier"]);
+            _clientFactory = clientFactory;
         }
 
-        List<JsonNode?> bases = menu["baseTable"].AsArray().ToList();
-        foreach (var obj in bases)
+        public async Task OnGetAsync()
         {
-            Sauces.Add((string)obj["option"], (double)obj["price"]);
+            var client = _clientFactory.CreateClient("localhost");
+            string response = await client.GetStringAsync("menu");
+            JsonObject? menu = JsonNode.Parse(response).AsObject();
+
+            var sizes = menu["sizeTable"].AsArray();
+            foreach (var obj in sizes)
+            {
+                Sizes.Add((string)obj["option"], (double)obj["multiplier"]);
+            }
+
+            var bases = menu["baseTable"].AsArray();
+            foreach (var obj in bases)
+            {
+                Sauces.Add((string)obj["option"], (double)obj["price"]);
+            }
+
+            var dough = menu["doughTable"].AsArray();
+            foreach (var obj in dough)
+            {
+                Dough.Add((string)obj["option"], (double)obj["price"]);
+            }
+
+            var toppings = menu["toppingTable"].AsArray();
+            foreach (var obj in toppings)
+            {
+                Toppings.Add((string)obj["option"], (double)obj["price"]);
+            }
         }
 
-        List<JsonNode?> dough = menu["doughTable"].AsArray().ToList();
-        foreach (var obj in dough)
+        public async Task OnPostAsync(Order myOrder)
         {
-            Dough.Add((string)obj["option"], (double)obj["price"]);
+        
         }
-
-        List<JsonNode?> toppings = menu["toppingTable"].AsArray().ToList();
-        foreach (var obj in toppings)
-        {
-            Toppings.Add((string)obj["option"], (double)obj["price"]);
-        }
-
     }
 }
