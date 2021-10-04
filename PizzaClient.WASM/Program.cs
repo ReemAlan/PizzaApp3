52689427f19361a6dd5b976fe8055241530d1646
Reem Alansary <reema@silverkeytech.com>
2021-10-04 17:15:12 +0200

    Finished GET request

--- PizzaClient.WASM/Program.cs
+++ PizzaClient.WASM/Program.cs
@@ -6,11 +6,21 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Grpc.Net.Client;
 using Grpc.Net.Client.Web;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddBlazorise( options =>
+{
+    options.ChangeTextOnKeyPress = true;
+} )
+.AddBootstrapProviders()
+.AddFontAwesomeIcons();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
