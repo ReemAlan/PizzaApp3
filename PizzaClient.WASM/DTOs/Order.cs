dac082537d9e503165d3a7327d689a37fd5cc531
Reem Alansary <reema@silverkeytech.com>
2021-10-08 13:39:38 +0200

    Fixed yesterday's errors

--- PizzaClient.WASM/DTOs/Order.cs
+++ PizzaClient.WASM/DTOs/Order.cs
@@ -4,25 +4,25 @@ using Google.Protobuf.Collections;
 
 namespace PizzaClient.WASM.DTOs
 {
-    public class Order
+    public class OrderDTO
     {
         [Required(ErrorMessage = "You have not chosen any pizza!")]
-        public Pizza Pizza { get; set; } = new();
+        public PizzaDTO Pizza { get; set; } = new();
         [Required(ErrorMessage = "Please enter your name")]
-        public string CustomerName { get; set; }
+        public string? CustomerName { get; set; }
     }
 
-    public class Pizza
+    public class PizzaDTO
     {
         [Required(ErrorMessage = "Please pick a size")]
-        public string Size { get; set; }
+        public string? Size { get; set; }
         [Required(ErrorMessage = "Please pick a dough type")]
-        public string Dough { get; set; }
+        public string? Dough { get; set; }
         [Required(ErrorMessage = "Please pick toppings")]
         public RepeatedField<string> Toppings { get; set; } = new();
         [Required(ErrorMessage = "Please pick a sauce")]
-        public string Sauce { get; set; }
+        public string? Sauce { get; set; }
         [Required]
-        public double Price { get; set; }
+        public double? Price { get; set; }
     }
 }
